6be546fdb44d9e15049079a223750a8cf893931c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-15 21:12:18 -0500

    :recycle: Update

--- server.ts
+++ server.ts
@@ -2,23 +2,47 @@ import { serve, Handler } from "https://deno.land/std@0.138.0/http/server.ts";
 
 const app = Deno.readTextFileSync("app.html");
 
-const handler: Handler = (request) => {
-  console.log(request);
+const handler: Handler = async (request) => {
+  if (request.method !== "GET" && request.method !== "POST") {
+    return new Response("Method not allowed.", { status: 400 });
+  }
+
+  const { pathname } = new URL(request.url);
 
   const headers: Record<string, string> = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Request-Method": "*",
-    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Methods": "GET, POST",
     "Access-Control-Allow-Headers": "*",
   };
 
-  if (request.method !== "GET") {
-    return new Response("Method not allowed.", { status: 400 });
+  if (pathname === "/") {
+    headers["content-type"] = "text/html";
+
+    return new Response(app, { headers, status: 200 });
   }
 
-  headers["content-type"] = "text/html";
+  if (pathname === "/api") {
+    if (!request.body) {
+      return new Response("Body is required.");
+    }
 
-  return new Response(app, { headers, status: 200 });
+    const reader = request.body.getReader();
+
+    const {value} = await reader.read();
+
+    if (!value) {
+      return new Response("No value on body.");
+    }
+
+    const text = new TextDecoder().decode(value);
+
+    console.log(text);
+
+    return new Response("4");
+  }
+
+  return new Response("Route not found.", { status: 404 });
 };
 
 serve(handler);
